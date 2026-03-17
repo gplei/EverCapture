742e42cc0d80b6c887dd5bef999871df53c2b3c9
Guangping Lei <leiabc@gmail.com>
2026-03-17 14:42:08 -0700

    get port from .env

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ const serverHost = process.env.SERVER_HOST || 'localhost';//'pingmac.local';
 const serverOrigin = serverHost.startsWith('http://') || serverHost.startsWith('https://')
     ? serverHost
     : `http://${serverHost}`;
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 const app = express();
 // const { json: _json } = pkg;
